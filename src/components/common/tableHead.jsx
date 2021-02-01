aaca1b4560a8185bcca882b22ee46754057937f7
Xopper <ali.haloua@usmba.ac.ma>
2021-02-01 18:55:58 +0100

    add sort icons to tHead

--- src/components/common/tableHead.jsx
+++ src/components/common/tableHead.jsx
@@ -9,6 +9,14 @@ class tableHead extends Component {
 		this.props.onSort(sortColumn);
 	};
 
+	renderSortIcon = column => {
+		const { sortColumn } = this.props;
+		if (column.path !== sortColumn.path) return null;
+		else if (sortColumn.order === "asc")
+			return <i className="fa fa-sort-asc"></i>;
+		return <i className="fa fa-sort-desc"></i>;
+	};
+
 	render() {
 		return (
 			<thead>
@@ -21,7 +29,7 @@ class tableHead extends Component {
 							}
 							scope="col"
 						>
-							{column.label}
+							{column.label} {this.renderSortIcon(column)}
 						</th>
 					))}
 				</tr>
